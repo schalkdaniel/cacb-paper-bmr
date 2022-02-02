7f5031d6896488d6a555f1d604690f8ce69afbfa
schalkdaniel <d-schalk@t-online.de>
2022-02-02 12:24:59 +0100

    updates

---
+++ simulations/benchmark.R
@@ -0,0 +1,5 @@
+# To conduct the simulation, run the `sim-code/simulate.R` script.
+
+# NOTE: This takes multiple weeks depending on the machine. The results are about ~11 GB and hence too big for GitHub. All results are uploaded in a Docker image and can be downloaded there.
+
+if (FALSE) source("sim-code/simulate.R")
